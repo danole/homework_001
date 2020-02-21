d955bcc6030724561eedb8aeb01f27e45ec87ccd
Ilya Frolov <1990boot1990@gmail.com>
2020-02-21 08:50:18 +0300

    Сделал задание 4

--- index.php
+++ index.php
@@ -35,3 +35,24 @@ elseif ($age>=1 && $age<=17){
 else{
     echo "Неизвестный возраст";
 }
+
+//Задание #4
+
+$day=3;
+
+switch ($day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo "Это рабочий день";
+        break;
+    case 6:
+    case 7:
+        echo "Это выходной день";
+        break;
+    default:
+        echo "Неизвестный день";
+        break;
+}
