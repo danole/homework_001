7c42a989b941c1d91657ec9950e8b65a48e0f6d7
Ilya Frolov <1990boot1990@gmail.com>
2020-02-21 08:24:52 +0300

    Сделал задание 2

--- index.php
+++ index.php
@@ -10,3 +10,10 @@ echo "<br>";
 echo "Мне: " . $age . " лет.";
 echo "<br>";
 echo "- “!|/’”\\";
+
+//Задание #2
+
+CONST pictures=80;
+CONST flomaster=23;
+CONST pencil=40;
+CONST paint=pictures-flomaster-pencil;
