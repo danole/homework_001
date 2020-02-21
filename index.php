c15573419878ccab45dc9e32f25c65b777791d0c
Ilya Frolov <1990boot1990@gmail.com>
2020-02-21 08:36:12 +0300

    Сделал задание 3

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ echo "<br>";
 echo "Мне: " . $age . " лет.";
 echo "<br>";
 echo "- “!|/’”\\";
+echo "<br>";
 
 //Задание #2
 
@@ -17,3 +18,20 @@ CONST pictures=80;
 CONST flomaster=23;
 CONST pencil=40;
 CONST paint=pictures-flomaster-pencil;
+
+//Задание #3
+
+$age=29;
+
+if ($age>=18 && $age<=65){
+    echo "Вам еще работать и работать";
+}
+elseif ($age>65){
+    echo "Вам пора на пенсию";
+}
+elseif ($age>=1 && $age<=17){
+    echo "Вам ещё рано работать";
+}
+else{
+    echo "Неизвестный возраст";
+}
